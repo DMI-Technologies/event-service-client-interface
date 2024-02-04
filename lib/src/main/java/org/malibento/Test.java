d5b9a3ef44280c536e6177f6c018c10a4edb50ec
Mihai <dimamihaimail@gmail.com>
2024-02-04 20:03:38 +0200

    added a test file

---
+++ lib/src/main/java/org/malibento/Test.java
@@ -0,0 +1,11 @@
+class Test {
+    private String value;
+
+    public String getValue() {
+        return this.value;
+    }
+
+    private void setValue(final String value) {
+        this.value = value;
+    }
+}
