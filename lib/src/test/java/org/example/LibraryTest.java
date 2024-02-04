8d60946b414225b1c229b31a2edef26f241d61df
Mihai <dimamihaimail@gmail.com>
2024-02-04 21:40:59 +0200

    fix missing library

--- lib/src/test/java/org/example/LibraryTest.java
+++ lib/src/test/java/org/example/LibraryTest.java
@@ -8,7 +8,5 @@ import static org.junit.Assert.*;
 
 public class LibraryTest {
     @Test public void someLibraryMethodReturnsTrue() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
